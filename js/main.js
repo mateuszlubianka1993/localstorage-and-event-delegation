f73c89c4fd874cd0f963a683955fc747fb056a02
Mateusz Łubianka <lubiankamateusz@gmail.com>
2018-12-12 13:05:49 +0100

    add html and css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-   
+    const addItems = document.querySelector('.add-items');
+    const itemsList = document.querySelector('.plates');
+    
 });
